cd3fc60e396f97060d112a5e4ddde5bf09e06bde
kazukiomori <kazuki1110170501@gmail.com>
2023-07-01 23:00:39 +0900

    駒の状態で管理するenumを作成

--- Othello/View/BanCell.swift
+++ Othello/View/BanCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class BanCell: UICollectionViewCell {
     
+    let status: BanStatus = .none
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
     func update() {
     
     }
---
+++ Othello/View/BanStatus.swift
@@ -0,0 +1,14 @@
+//
+//  BanStatus.swift
+//  Othello
+//
+//  Created by Kazuki Omori on 2023/07/01.
+//
+
+import Foundation
+
+enum BanStatus {
+    case none
+    case black
+    case white
+}
