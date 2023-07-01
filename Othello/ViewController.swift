dbaafb302bcaa2c4f1a091d317401523424c6559
kazukiomori <kazuki1110170501@gmail.com>
2023-07-01 20:56:53 +0900

    Initial Commit

---
+++ Othello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Othello
+//
+//  Created by Kazuki Omori on 2023/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
