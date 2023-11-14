ff42ca9230a6622e268a8e1e7f54ca710738badb
kazukiomori <kazuki1110170501@gmail.com>
2023-11-15 03:56:42 +0900

    [Add]マス目の色を判定する関数のテストコードを作成

--- OthelloTests/OthelloTests.swift
+++ OthelloTests/OthelloTests.swift
@@ -16,20 +16,10 @@ final class OthelloTests: XCTestCase {
         XCTAssertNil(Position(x: 0, y: 8))
         XCTAssertNil(Position(x: -1, y: 0))
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_マス目のテスト() throws {
+        XCTAssertEqual(FieldViewController().getStatus(position: .init(x: 0, y: 0)!), .空)
+        XCTAssertEqual(FieldViewController().getStatus(position: .init(x: 3, y: 3)!), .白)
+        XCTAssertEqual(FieldViewController().getStatus(position: .init(x: 3, y: 4)!), .黒)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
