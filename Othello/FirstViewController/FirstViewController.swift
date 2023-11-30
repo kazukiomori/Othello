766ff99ee75f3e52be9712ecad73016dca6d0756
kazukiomori <kazuki1110170501@gmail.com>
2023-12-01 02:59:06 +0900

    [Add]最初の画面を作成

---
+++ Othello/FirstViewController/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Othello
+//
+//  Created by Kazuki Omori on 2023/12/01.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+   
+    @IBAction func tappedOffline(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedComputer(_ sender: Any) {
+        
+    }
+    
+}
